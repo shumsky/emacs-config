51d9a3f0872b2bad7f6223ab665c22db29101264
Anton Shumsky <anton.shumsky@gmail.com>
2017-05-25 12:46:18 +0300

    add tabbar

--- init.el
+++ init.el
@@ -1,7 +1,10 @@
 (require 'package)
 (add-to-list 'load-path "~/.emacs.d/lisp/")
+(add-to-list 'load-path "~/.emacs.d/lisp/tabbar")
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/"))
+
+(require 'tabbar)
 
 (setq-default truncate-lines t)
 (setq-default indent-tabs-mode nil)
@@ -10,6 +13,7 @@
 
 (tool-bar-mode -1)
 (global-auto-revert-mode 1)
+(show-paren-mode 1)
 
 (global-set-key [f8] 'neotree-toggle)
 (setq neo-theme 'arrow)
